84e67cd5e5ccd32ed767aa561b6c51a6009634e1
Jennilyn Howell <jennilynhowell@me.com>
2017-07-27 09:54:12 -0400

    Initial commit

---
+++ src/main/java/com/jennilyn/FirstspringApplication.java
@@ -0,0 +1,12 @@
+package com.jennilyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringApplication.class, args);
+	}
+}

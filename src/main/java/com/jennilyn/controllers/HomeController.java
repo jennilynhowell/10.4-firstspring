bb213c13156294d4e5ae133a32f17d00492f33ac
Jennilyn Howell <jennilynhowell@me.com>
2017-07-27 11:41:44 -0400

    Implemented thymeleaf templating

--- src/main/java/com/jennilyn/controllers/HomeController.java
+++ src/main/java/com/jennilyn/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package com.jennilyn.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(@RequestParam(value = "data", required = false) String data,
+                        @RequestParam(value = "data2", required = false) String data2,
+                        ModelMap model){
+        System.out.println(data);
+        System.out.println(data2);
+
+        model.addAttribute("pageData", data + " " + data2);
         return "index";
     }
 

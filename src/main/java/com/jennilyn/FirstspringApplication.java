916d52f201fe0e026337305131dcfdf95b3aeabc
Jennilyn Howell <jennilynhowell@me.com>
2017-07-27 11:02:04 -0400

    Added controller and template for index page.

--- src/main/java/com/jennilyn/FirstspringApplication.java
+++ src/main/java/com/jennilyn/FirstspringApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstspringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FirstspringApplication.class, args);
+
+
+
 	}
 }
